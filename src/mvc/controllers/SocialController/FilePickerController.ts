08e515c9ba162a379d3077935822216f8ca2f936
sevensevener <1810653109@stud.fh-kufstein.ac.at>
2019-08-01 20:26:13 +0200

    photo

--- src/mvc/controllers/SocialController/FilePickerController.ts
+++ src/mvc/controllers/SocialController/FilePickerController.ts
@@ -4,30 +4,14 @@ export const openFilePicker = (cb: (res: ImagePickerResponse) => void): void => 
     const options: ImagePickerOptions = {
         title: "Beweis es!",
         mediaType: "photo", // works only on ios, on android only photos are used, TODO: add video support (but base64 might not work here and pay attention to which platform supports which attributes -> path, uri, ...)
-        customButtons: [{ name: "video", title: "Make a video..." }],
         storageOptions: {
             skipBackup: true,
+            path: "photo",
         },
         // noData: true, // TODO: uncomment when pic/vid shared via path as this greatly enhances performance!
     }
 
-
-    const videoOptions: any = {
-        storageOptions: {
-            skipBackup: true,
-        },
-        mediaType: "video",
-        noData: true,
-    }
-
     ImagePicker.showImagePicker(options, (res: ImagePickerResponse) => {
-        if (res.customButton === "video") {
-            ImagePicker.launchCamera(videoOptions, res => {
-                console.log("user wants to make a video")
-                cb(res)
-            })
-        } else {
-            cb(res)
-        }
+        cb(res)
     })
 }
--- src/mvc/controllers/SocialController/ShareController.ts
+++ src/mvc/controllers/SocialController/ShareController.ts
@@ -2,35 +2,27 @@ import { ImagePickerResponse } from "react-native-image-picker"
 import Share from "react-native-share"
 
 export const shareImage = async (headline: string, sponsorName: string, res: ImagePickerResponse, cb?: (wasShareSuccessful: boolean) => void) => {
-
     // options for sharing an image
-    const shareOptionsImage = {
+    const shareOptions = {
         title: "Share via",
         message: `Hey Leute, ich habe die Challenge "${headline}" (Gesponsort von "${sponsorName}") erfolgreich gelöst, hier der Beweis:`,
         url: `data:${res.type};base64, ${res.data}`,
     }
 
-    // options for sharing a video
-    const shareOptionsVideo = {
-        title: "Check out my video",
-        message: "Check out my video!",
-        url: `file://${res.path}`,
-        type: "video/mp4",
-        subject: "Check out my video!",
-    }
-
-    const shareOptions = res.type === "image/jpeg" ? shareOptionsImage : shareOptionsVideo
-
-    console.log(shareOptions)
+    console.log("Options", cb)
 
     Share.open(shareOptions)
         .then((_: any) => {
+            console.log("fuck you")
             if (cb) {
+                console.log("fuck 1")
                 cb(true)
             }
         })
         .catch((err: any) => {
+            console.log("fuck me")
             if (cb) {
+                console.log("fuck 2")
                 cb(false)
             }
             console.warn("ShareController:shareMedia: Action aborted by user -> " + JSON.stringify(err))
