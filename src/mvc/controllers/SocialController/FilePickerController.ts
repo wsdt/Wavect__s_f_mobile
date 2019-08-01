e537d3b271dd82aa7df691f0f024a50e26b160f8
wsdt <kevin.riedl.privat@gmail.com>
2019-08-01 18:11:49 +0200

    h

--- src/mvc/controllers/SocialController/FilePickerController.ts
+++ src/mvc/controllers/SocialController/FilePickerController.ts
@@ -1,7 +1,7 @@
-import ImagePicker, {ImagePickerOptions, ImagePickerResponse} from "react-native-image-picker"
+import ImagePicker, { ImagePickerOptions, ImagePickerResponse } from "react-native-image-picker"
 
 export const openFilePicker = (cb: (res: ImagePickerResponse) => void): void => {
-    const options:ImagePickerOptions = {
+    const options: ImagePickerOptions = {
         title: "Beweis es!",
         mediaType: "photo", // works only on ios, on android only photos are used, TODO: add video support (but base64 might not work here and pay attention to which platform supports which attributes -> path, uri, ...)
         storageOptions: {
@@ -10,8 +10,7 @@ export const openFilePicker = (cb: (res: ImagePickerResponse) => void): void => 
         // noData: true, // TODO: uncomment when pic/vid shared via path as this greatly enhances performance!
     }
 
-    ImagePicker.showImagePicker(options, (res:ImagePickerResponse) => {
+    ImagePicker.showImagePicker(options, (res: ImagePickerResponse) => {
         cb(res)
     })
 }
-
--- src/mvc/controllers/SocialController/ShareController.ts
+++ src/mvc/controllers/SocialController/ShareController.ts
@@ -1,8 +1,7 @@
-import {ImagePickerResponse} from "react-native-image-picker"
+import { ImagePickerResponse } from "react-native-image-picker"
 import Share from "react-native-share"
 
 export const shareImage = (headline: string, sponsorName: string, res: ImagePickerResponse, cb?: (wasShareSuccessful: boolean) => void) => {
-
     const shareOptions = {
         title: "Share via",
         message: `Hey Leute, ich habe die Challenge "${headline}" (Gesponsort von "${sponsorName}") erfolgreich gelöst, hier der Beweis:`,
